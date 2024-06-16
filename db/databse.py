f5fa70d4b39be0e14f8f8719bec9aebe6df3fb6e
Library Keeper <libraraykeeperpiegames@gmail.com>
2024-06-16 18:11:17 +0300

    Change DB from SQLite to MySQL

--- db/databse.py
+++ db/databse.py
@@ -1,3 +1,3 @@
-from peewee import SqliteDatabase
+from playhouse.db_url import connect
 
-db = SqliteDatabase("PieAnimeDB.db")
+db = connect("mysql://u3077_ycoEubxey0:.L2!tH9UK!hzyX3lr@10H.VP@mysql.play2go.cloud:3306/s3077_PieAnimeDB")
