67b9c9cf4ddc6695bba55c6d2116ade23d75afd2
Library Keeper <libraraykeeperpiegames@gmail.com>
2024-06-16 18:15:06 +0300

    Im stupid - replace link to .env

--- db/databse.py
+++ db/databse.py
@@ -1,3 +1,5 @@
 from playhouse.db_url import connect
-
-db = connect("mysql://u3077_ycoEubxey0:.L2!tH9UK!hzyX3lr@10H.VP@mysql.play2go.cloud:3306/s3077_PieAnimeDB")
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
+db = connect(getenv("DB_URL"))
